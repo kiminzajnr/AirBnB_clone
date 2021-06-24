2afbc6e2d93b64d4bcedbe7884f1b41de147da2b
Erick Kiminza <kiminzaeric@gmail.com>
2021-06-24 21:09:23 +0300

    starter code for base model tests"

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,10 @@
+"""
+This module tests the base class
+"""
+import unittest
+
+
+class TestBaseModel(unittest.TestCase):
+    """tests for the base class"""
+if __name__ == "__main__":
+    unittest.main()
