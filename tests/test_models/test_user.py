14a1708cc1dbc715b38d2fc766f826b0bcba491c
Erick Kiminza <kiminzaeric@gmail.com>
2021-06-24 21:10:19 +0300

    starter code for User model tests

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,11 @@
+"""
+This module test the User class
+"""
+import unittest
+
+
+class TestUser(unittest.TestCase):
+    """Test for the user class that
+    inherits from BaseModel"""
+if __name__ == "__main__":
+    unittest.main()
