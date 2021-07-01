c9fbb4c8bd24e3bbe2cc3ed2d2bea278e69a5f12
Erick Kiminza <kiminzaeric@gmail.com>
2021-07-01 16:16:31 +0300

    fix import error

--- console.py
+++ console.py
@@ -2,12 +2,24 @@
 """Command interpreter.
 """
 import cmd
+import models
 
 
 class HBNBCommand(cmd.Cmd):
     """Command iterpreter public class which inherites from base class cmd.
     """
     prompt = '(hbnb) '
+
+    def do_create(self, line):
+        """creates a new instance of BaseModel, saves it
+        (to the json file) and prints the id
+        """
+        if len(line) == 0:
+            print("** class name missing **")
+        
+        new_instance = models.base_model.BaseModel()
+        print(new_instance.id)
+        new_instance.save()
 
     def do_quit(self, line):
         """Quit command to exit the program
@@ -25,7 +37,6 @@ class HBNBCommand(cmd.Cmd):
         execute the previous command, with just do nothing.
         """
         pass
-
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
